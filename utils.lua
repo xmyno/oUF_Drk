d31afbfc6f74efc39981f14efe13ac3dbc3edc82
xmyno <xmynox@gmail.com>
2018-05-01 23:38:51 +0200

    Add workaround for UnitBuff/UnitDebuff spellId matching

---
+++ utils.lua
@@ -0,0 +1,22 @@
+local addon, ns = ...
+local utils = CreateFrame("Frame")
+
+local UnitBuff, UnitDebuff = UnitBuff, UnitDebuff
+
+function utils.UnitBuff(unit, spellId)
+	for i = 1, 40 do
+		local _, _, _, _, _, _, _, _, id = UnitBuff(unit, i)
+		if not id then return end
+		if (spellId == id) then return UnitBuff(unit, i) end
+	end
+end
+
+function utils.UnitDebuff(unit, spellId)
+	for i = 1, 40 do
+		local _, _, _, _, _, _, _, _, id = UnitDebuff(unit, i)
+		if not id then return end
+		if (spellId == id) then return UnitDebuff(unit, i) end
+	end
+end
+
+ns.utils = utils
