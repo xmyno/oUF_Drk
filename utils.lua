d9e708e7642d831091a5c37da08b2c01059d36e7
xmyno <xmynox@gmail.com>
2018-07-27 23:09:43 +0200

    Fix aura queries

--- utils.lua
+++ utils.lua
@@ -5,7 +5,7 @@ local UnitBuff, UnitDebuff = UnitBuff, UnitDebuff
 
 function utils.UnitBuff(unit, spellId)
 	for i = 1, 40 do
-		local _, _, _, _, _, _, _, _, id = UnitBuff(unit, i)
+		local _, _, _, _, _, _, _, _, _, id = UnitBuff(unit, i)
 		if not id then return end
 		if (spellId == id) then return UnitBuff(unit, i) end
 	end
@@ -13,7 +13,7 @@ end
 
 function utils.UnitDebuff(unit, spellId)
 	for i = 1, 40 do
-		local _, _, _, _, _, _, _, _, id = UnitDebuff(unit, i)
+		local _, _, _, _, _, _, _, _, _, id = UnitDebuff(unit, i)
 		if not id then return end
 		if (spellId == id) then return UnitDebuff(unit, i) end
 	end
